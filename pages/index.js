5b242731d0ec6581329d9460994fdc85a9e36c8e
Sean Anih <sean.anih@gmail.com>
2023-04-15 13:34:59 -0400

    fixed off-center text on index page

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ export default function Home() {
 			</Head>
 
 			<main className='flex flex-col rounded-xl justify-center items-center py-8 w-[80%] md:w-[70%] bg-gray-300 text-black'>
-				<h1 className='mb-4'>
+				<h1 className='mb-4 text-center'>
 					Welcome to{' '}
 					<span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-gray-700 via-cyan-600 to-gray-700'>
 						Smart Bank
 					</span>
 				</h1>
-				<p>Please sign in with a Web3 account to begin using our services</p>
+				<p className='px-4 text-center'>Please sign in with a Web3 account to begin using our services</p>
 				<Link
 					href={'/signin'}
 					className='py-2 mt-5 btn hover:bg-gradient-to-br from-gray-700 via-cyan-600 to-gray-700 underline'
